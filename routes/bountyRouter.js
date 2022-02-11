c3aa2d0047a7e596b18e09e5d4fcd496c8a9f3c4
Adrian Walker <awalker3365@gmail.com>
2022-02-11 15:36:48 -0500

    added mongodb

--- routes/bountyRouter.js
+++ routes/bountyRouter.js
@@ -1,47 +1,57 @@
 const express = require('express')
-const { v4: uuidv4 } = require('uuid');
+// const { v4: uuidv4 } = require('uuid');
 const bountyRouter = express.Router()
+const Bounty = require('../models/bounties.js')
 
 //Bounty Names
-const bounties = [
-    { firstname: "Willie", lastname: "Jenkins", living: "true", bounty: "5,200", type: "Sith", _id: uuidv4() },
-    { firstname: "Jeff", lastname: "Jones", living: "true", bounty: "50", type: "Jedi", _id: uuidv4() },
-    { firstname: "Kevin", lastname: "Anderson", living: "true", bounty: "190,000", type: "Jedi", _id: uuidv4() },
-    { firstname: "Daffy", lastname: "Duck", living: "false", bounty: "5,000,000", type: "Sith", _id: uuidv4() },
-    { firstname: "Elroy", lastname: "Jetson", living: "false", bounty: "45,000", type: "Jedi", _id: uuidv4() },
+// const bounties = [
+//     { firstname: "Willie", lastname: "Jenkins", living: "true", bounty: "5,200", type: "Sith", _id: uuidv4() },
+//     { firstname: "Jeff", lastname: "Jones", living: "true", bounty: "50", type: "Jedi", _id: uuidv4() },
+//     { firstname: "Kevin", lastname: "Anderson", living: "true", bounty: "190,000", type: "Jedi", _id: uuidv4() },
+//     { firstname: "Daffy", lastname: "Duck", living: "false", bounty: "5,000,000", type: "Sith", _id: uuidv4() },
+//     { firstname: "Elroy", lastname: "Jetson", living: "false", bounty: "45,000", type: "Jedi", _id: uuidv4() },
 
-]
+// ]
 
 //Routes
 
 //get all
-bountyRouter.get("/", (req, res) => {
-    res.send(bounties)
+bountyRouter.get("/", (req, res, next) => {
+    Bounty.find((err, bounties) => {
+        if (err) {
+            res.status(500)
+            return next(err)
+        }
+        return res.status(200).send(bounties)
+    })
 })
 //get one
-bountyRouter.get("/:bountyId", (req, res) => {
-    const bountyId = req.params.bountyId
-    const foundBounty = bounties.find(bounty => bounty._id === bountyId)
-    console.log("Found bounty is " + foundBounty)
-    res.send(foundBounty)
-})
+// bountyRouter.get("/:bountyId", (req, res) => {
+//     const bountyId = req.params.bountyId
+//     const foundBounty = bounties.find(bounty => bounty._id === bountyId)
+//     console.log("Found bounty is " + foundBounty)
+//     res.send(foundBounty)
+// })
 
 //get by type
-bountyRouter.get("/search/type", (req, res) => {
-    const type = req.query.type
-    const filteredBounties = bounties.filter(bounty => bounty.type === type)
-    res.send(filteredBounties)
-    console.log(filteredBounties)
+// bountyRouter.get("/search/type", (req, res) => {
+//     const type = req.query.type
+//     const filteredBounties = bounties.filter(bounty => bounty.type === type)
+//     res.send(filteredBounties)
+//     console.log(filteredBounties)
 
-})
+// })
 
 //post one
-bountyRouter.post("/", (req, res) => {
-    console.log(req.body)
-    const newBounty = req.body
-    newBounty._id = uuidv4()
-    bounties.push(newBounty)
-    res.send(newBounty)
+bountyRouter.post("/", (req, res, next) => {
+    const newBounty = new Bounty(req.body)
+    newBounty.save((err,savedBounty) => {
+        if (err) {
+            res.status(500)
+            return next(err)
+        }
+        return res.status(201).send(savedBounty)
+    })
 })
 
 //delete one
--- server.js
+++ server.js
@@ -1,16 +1,34 @@
 const express = require("express")
 const app = express()
 const morgan = require("morgan")
+const mongoose = require('mongoose')
 
 //Middleware for every response
 app.use(express.json())
 app.use(morgan("dev"))
+
+//Connect To DB
+mongoose.connect('mongodb://localhost:27017/bountydb',
+    {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+        useCreateIndex: true,
+        useFindAndModify: false
+    },
+    () => console.log('Connected To The Database!')
+)
+
+//Error Handler
+app.use((err, req, res, next) => {
+    console.log(err)
+    return res.send({ erMsg: err.message })
+})
 
 //routes
 app.use("/bounties", require("./routes/bountyRouter.js"))
 //app.use("/tvshows", require("./routes/tvshowRouter.js"))
 
 // Server Listen
-app.listen(9000, () =>{
+app.listen(9000, () => {
     console.log("The server is running on port 9000")
 })
